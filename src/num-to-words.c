5f789d4dd275d8de9fc9684f6294153eaea37f07
Ayan Shafqat <ayan.x.shafqat@gmail.com>
2024-12-07 20:30:25 -0500

    Add header

--- src/num-to-words.c
+++ src/num-to-words.c
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2024 Ayan Shafqat <ayan@shafq.at>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "check-properties.h" /* Declares num_to_words () */
+
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
